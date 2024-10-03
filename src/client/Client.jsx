7cdd3e8dd77e241ac498091a87f9849293409d74
Stacy Bridges <stcybrdgs@gmail.com>
2024-10-03 16:40:00 -0500

    finished progress bar events on audio player

--- src/client/Client.jsx
+++ src/client/Client.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState, useRef } from 'react'
 import './client.scss'
 import albums from '../data/albums.js'
@@ -30,33 +31,98 @@ Lorem ipsum odor amet, consectetuer adipiscing elit. Euismod suspendisse arcu ph
 
 const Client = () => {
   const [selectedSong, setSelectedSong] = useState({})
-  const [songIsPlaying, setSongIsPlaying] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
   const [selectedAlbum, setSelectedAlbum] = useState({})
   const [radioMenuSongs, setRadioMenuSongs] = useState([])
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+  const [isShuffled, setIsShuffled] = useState(false)
+  const [isRepeated, setIsRepeated] = useState(false)
+  const [progressBarWidth, setProgressBarWidth] = useState('0%')
+
+  const playerStyles = {
+    progressBar: { width: progressBarWidth },
+    playBtn: { display: isPlaying ? 'none' : 'inline' },
+    pauseBtn: { display: isPlaying ? 'inline' : 'none' },
+    shuffleBtn: {
+      opacity: isShuffled ? '1' : '0.6',
+      textShadow: isShuffled ? '0px 0px 10px #fff' : 'none',
+    },
+    repeatBtn: {
+      opacity: isRepeated ? '1' : '0.6',
+      textShadow: isRepeated ? '0px 0px 10px #fff' : 'none',
+    },
+  }
 
   const audioRef = useRef(null)
-  const audioPlayBtnRef = useRef(null)
+  //const songProgressRef = useRef()
+  // const audioPlayBtnRef = useRef(null)
+  // const audioPauseBtnRef = useRef(null)
 
-  // API helpers
-  const callApiForDefaultAlbumData = () => {
-    return albums[0]
+  // get timestamp for progressbar
+  const getTimestamp = (time) => {
+    let minutes = Math.floor(time / 60)
+    let seconds = Math.floor(time % 60)
+    if (minutes < 10) {
+      minutes = `0${minutes}`
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`
+    }
+    return `${minutes}:${seconds}`
   }
 
   // Player Control Functions
-  const togglePlaySong = () => {
-    if (songIsPlaying) {
-      console.log('pause')
-      audioRef.current.pause()
-    } else {
-      console.log('play')
-      audioRef.current.play()
+  const handleAudioTimeUpdate = () => {
+    if (audioRef.current) {
+      console.log('handle Audio Time Update')
+      const currentTimeInMs = audioRef.current.currentTime
+      const durationInMs = audioRef.current.duration
+      const progressRatio =
+        currentTimeInMs === 0 ? 0 : currentTimeInMs / durationInMs
+
+      // set progress-bar width in the player ui
+      setProgressBarWidth(`${Math.round(progressRatio * 100)}%`)
+
+      // update current timestamp in player ui
+      const currentTime = getTimestamp(currentTimeInMs)
+      setCurrentTime(currentTime)
     }
-    setSongIsPlaying(!songIsPlaying)
-    console.log('audio ref:', audioRef.current)
+  }
+
+  const handleAudioLoadedMetadata = () => {
+    if (audioRef.current) {
+      const currentTime = getTimestamp(audioRef.current.duration)
+      setDuration(currentTime)
+    }
+  }
+
+  const handleAudioEnded = () => {
+    nextSong()
+  }
+
+  const handleAudioLoadedData = () => {
+    console.log('loaded data')
+    if (isPlaying) {
+      playSong()
+    }
+  }
+
+  // const handleTimeUpdate = () => {
+  //   console.log('time update')
+  // }
+
+  const playSong = () => {
+    audioRef.current.play()
+    setIsPlaying(true)
+  }
+
+  const pauseSong = () => {
+    audioRef.current.pause()
+    setIsPlaying(false)
   }
 
   const prevSong = () => {
-    console.log('prev')
     const currentIndex = selectedSong.track - 1
     const prevIndex =
       currentIndex === 0 ? radioMenuSongs.length - 1 : currentIndex - 1
@@ -64,10 +130,6 @@ const Client = () => {
 
     setSelectedSong(prevSong)
     loadSong(selectedSong)
-
-    if (songIsPlaying) {
-      audioRef.current.play()
-    }
   }
 
   const nextSong = () => {
@@ -76,41 +138,59 @@ const Client = () => {
       currentIndex === radioMenuSongs.length - 1 ? 0 : currentIndex + 1
     const nextSong = radioMenuSongs[nextIndex]
 
-    // console.log('next', currentIndex, nextIndex, nextSong)
     setSelectedSong(nextSong)
-    loadSong(selectedSong)
-
-    if (songIsPlaying) {
-      audioRef.current.play()
-    }
+    loadSong(nextSong)
   }
 
-  const shuffleSongs = () => {
+  const toggleShuffleSongs = () => {
     console.log('shuffle')
+    setIsShuffled(!isShuffled)
   }
+
   const repeatSong = () => {
     console.log('repeat')
+    setIsRepeated(!isRepeated)
   }
+
   const loadSong = (song) => {
-    console.log('load song: ', song)
     audioRef.src = song
+    handleAudioTimeUpdate()
+    handleAudioLoadedMetadata()
+  }
+
+  // Set song progress when user clicks on progress bar
+  const setSongProgress = (e) => {
+    const selectedProgress = e.nativeEvent.offsetX
+    const progressRatio = selectedProgress / e.target.parentNode.offsetWidth
+    const selectedTimeInMs = audioRef.current.duration * progressRatio
+
+    // set ms duration in the html audio controls
+    audioRef.current.currentTime = selectedTimeInMs
+
+    // set selected timestamp in the player ui
+    setCurrentTime(getTimestamp(selectedTimeInMs))
+
+    // set progress-bar width in the player ui
+    setProgressBarWidth(`${Math.round(progressRatio * 100)}%`)
+  }
+
+  // API helpers
+  const callApiForDefaultAlbumData = () => {
+    return albums[0]
   }
 
   useEffect(() => {
     const selectedAlbum = callApiForDefaultAlbumData()
-    setSelectedAlbum(selectedAlbum)
-
     const selectedSong = selectedAlbum.songs[0]
-    setSelectedSong(selectedSong)
-
     const radioMenuSongs = selectedAlbum.songs
+
+    setSelectedAlbum(selectedAlbum)
+    setSelectedSong(selectedSong)
     setRadioMenuSongs(radioMenuSongs)
 
     if (audioRef.current) {
       loadSong(selectedSong.fileUrl)
     }
-
-    console.log('selectedSong:', selectedSong)
   }, [])
 
   // return <h2>Test</h2>
@@ -190,22 +270,39 @@ const Client = () => {
 
             <div className='controls'>
               {/* Progress Bar */}
-              <div className='progress-bar'>
-                <div className='progress' />
+              <div className='progress-bar' onClick={setSongProgress}>
+                {/* <div className='progress' ref={songProgressRef} /> */}
+                <div
+                  className='progress'
+                  value={currentTime}
+                  max={duration}
+                  style={playerStyles.progressBar}
+                />
                 <div className='duration-wrapper'>
-                  <span id='current-time' />
-                  <span id='duration' />
+                  <span id='current-time'>{currentTime}</span>
+                  <span id='duration'>{duration}</span>
                 </div>
               </div>
 
-              <audio src={selectedSong.fileUrl} controls ref={audioRef}></audio>
+              <audio
+                type='audio/mpeg'
+                ref={audioRef}
+                src={selectedSong.fileUrl}
+                onTimeUpdate={handleAudioTimeUpdate}
+                onLoadedMetadata={handleAudioLoadedMetadata}
+                onLoadedData={handleAudioLoadedData}
+                onEnded={handleAudioEnded}
+                controls
+              />
+              {/* onTimeUpdate={handleTimeUpdate} */}
 
               {/* Control Buttons */}
               <div className='control-buttons'>
                 <i
                   className='fa-solid fa-shuffle'
-                  title='Shuffle'
-                  onClick={shuffleSongs}
+                  title='Shuffle songs'
+                  onClick={toggleShuffleSongs}
+                  style={playerStyles.shuffleBtn}
                 ></i>
 
                 <div className='main-control-buttons'>
@@ -215,14 +312,23 @@ const Client = () => {
                     title='Previous'
                     onClick={prevSong}
                   ></i>
-                  <div className='play-button-wrapper' onClick={togglePlaySong}>
+                  <div
+                    className='play-button-wrapper'
+                    onClick={isPlaying ? pauseSong : playSong}
+                  >
                     <i
-                      className={`fas ${
-                        songIsPlaying ? 'fa-pause' : 'fa-play'
-                      }`}
-                      id={songIsPlaying ? 'pause' : 'play'}
-                      title={songIsPlaying ? 'Pause' : 'Play'}
-                      ref={audioPlayBtnRef}
+                      className='fas fa-play'
+                      id='play'
+                      title='Play'
+                      onClick={playSong}
+                      style={playerStyles.playBtn}
+                    ></i>
+                    <i
+                      className='fas fa-pause'
+                      id='pause'
+                      title='Pause'
+                      onClick={pauseSong}
+                      style={playerStyles.pauseBtn}
                     ></i>
                   </div>
                   <i
@@ -234,8 +340,10 @@ const Client = () => {
                 </div>
                 <i
                   className='fa-solid fa-repeat'
-                  title='Repeat'
+                  id='repeat'
+                  title='Repeat songs'
                   onClick={repeatSong}
+                  style={playerStyles.repeatBtn}
                 ></i>
               </div>
 
